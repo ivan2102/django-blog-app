6b762d7e02b4acd9b9c5db120a2ab49d09edf452
Ivan <radisav80@yahoo.com>
2024-06-04 23:17:08 +0200

    adding postgresql database in settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'beograd011',
+        'HOST': 'localhost'
     }
 }
 
