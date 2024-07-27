3d9ad09abc6da0497aca630fe76d34aa158811f2
Ivan <radisav80@yahoo.com>
2024-07-27 18:54:36 +0200

    new changes in setting.py file

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR / 'upload')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
 MEDIA_URL = '/upload/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'django_blog/static')
